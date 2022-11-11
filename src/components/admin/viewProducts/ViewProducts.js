308bcbb785b67015959b5ca25c71734f7cbb9158
cheechqt <fqqwsj@gmail.com>
2022-11-11 20:50:26 +0400

    viewProducts + deleting them from storage and firestore

--- src/components/admin/viewProducts/ViewProducts.js
+++ src/components/admin/viewProducts/ViewProducts.js
@@ -1,7 +1,112 @@
-function ViewProducts() {
- return (
-   <div>ViewProduct</div>
- )
-};
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import {
+  collection,
+  onSnapshot,
+  orderBy,
+  query,
+  doc,
+  deleteDoc,
+} from "firebase/firestore";
+import { ref, deleteObject } from "firebase/storage";
+import { db, storage } from "../../../firebase/config";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+import Loader from "../../loader/Loader";
+import styles from "./ViewProducts.module.scss";
 
-export default ViewProducts
+function ViewProducts() {
+  const [products, setProducts] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+  const getProducts = () => {
+    setIsLoading(true);
+    try {
+      const colRef = collection(db, "products");
+      const q = query(colRef, orderBy("createdAt", "desc"));
+      onSnapshot(q, (snapshot) => {
+        const allProducts = snapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        console.log(allProducts);
+        setProducts(allProducts);
+        setIsLoading(false);
+      });
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+    }
+  };
+
+  const deleteProduct = async (id, imageUrl) => {
+    try {
+      await deleteDoc(doc(db, "products", id));
+      deleteObject(ref(storage, imageUrl));
+      toast.success("Product deleted successfully.");
+    } catch (error) {
+      toast.error(error.message);
+    }
+  };
+  return (
+    <>
+      {isLoading && <Loader />}
+      <div className={styles.table}>
+        <h2>All Products</h2>
+
+        {products.length === 0 ? (
+          <p>No products found</p>
+        ) : (
+          <table>
+            <thead>
+              <tr>
+                <th>s/n</th>
+                <th>Image</th>
+                <th>Name</th>
+                <th>Category</th>
+                <th>Price</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product, index) => {
+                const { id, name, price, imageUrl, category } = product;
+                return (
+                  <tr key={id}>
+                    <td>{index + 1}</td>
+                    <td>
+                      <img
+                        src={imageUrl}
+                        alt={name}
+                        style={{ width: "100px" }}
+                      />
+                    </td>
+                    <td>{name}</td>
+                    <td>{category}</td>
+                    <td>{`$${price}`}</td>
+                    <td className={styles.icons}>
+                      <Link to="/admin/add-product">
+                        <FaEdit size={20} color="green" />
+                      </Link>
+                      &nbsp;
+                      <FaTrashAlt
+                        size={18}
+                        color="red"
+                        onClick={() => deleteProduct(id, imageUrl)}
+                      />
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default ViewProducts;
