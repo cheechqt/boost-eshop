01fc940459cef8f97026723f42e9f54de2b7b6f8
cheechqt <fqqwsj@gmail.com>
2022-11-21 14:02:20 +0400

    deploy

--- src/pages/checkout/Checkout.js
+++ src/pages/checkout/Checkout.js
@@ -34,7 +34,8 @@ function Checkout() {
 
   useEffect(() => {
     // Create PaymentIntent as soon as the page loads
-    fetch("http://localhost:4242/create-payment-intent", {
+    // http://localhost:4242/create-payment-intent
+    fetch("https://e-sponge.herokuapp.com/create-payment-intent", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
